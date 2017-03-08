3ccc62a9fe3faf394c18deccec1b5e47dd7ee3a5
adrianjania <adrianjania543@gmail.com>
2017-03-08 12:52:50 -0600

    Added schools project, added ranking app, worked on models

---
+++ schools/ranking/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class School(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    description = models.TextField()
+    photo = models.ImageField(XXXXXXX)
+    def __str__(self):
+        return self.name
+
+class Enrollment(models.Model):
+    school = models.ForeignKey(School)
+    year = models.DateField()
+    total_enrollment = models.IntegerField()
+
+class TestType(models.Model):
+    type = models.CharField(max_length=255)
+    def __str__(self):
+        return self.type
+
+class TestScores(models.Model):
+    school = models.ForeignKey(School)
+    test_type = models.ForeignKey(TestType)
+    score = models.FloatField()
