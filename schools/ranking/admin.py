3c707ec2861b0dad116112d6613f1473f99b9074
Mollygeorge <jessica.george@huskers.unl.edu>
2017-03-30 16:11:32 -0500

    Added enrollment and changed year filed
    Added enrollment to admin page and changed year field under enrollment
    from date field to Character field.  Listed all schools and addresses

--- schools/ranking/admin.py
+++ schools/ranking/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from ranking.models import TestType, TestScores, School
+from ranking.models import TestType, TestScores, School, Enrollment
 # Register your models here.
 admin.site.register(TestType)
 admin.site.register(TestScores)
 admin.site.register(School)
+admin.site.register(Enrollment)
