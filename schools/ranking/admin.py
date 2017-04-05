d6926dad163fda7e2e4ebb5235bdf0e65053e9cb
Mollygeorge <jessica.george@huskers.unl.edu>
2017-04-05 16:39:19 -0500

    Added Activities Type to admin
    Added some sports to Activities type (cc, football, girls golf,
    volleyball, swimming)

--- schools/ranking/admin.py
+++ schools/ranking/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from ranking.models import TestType, TestScores, School, Enrollment, Activities
+from ranking.models import TestType, TestScores, School, Enrollment, ActivityType, Activities
 # Register your models here.
 admin.site.register(TestType)
 admin.site.register(TestScores)
 admin.site.register(School)
 admin.site.register(Enrollment)
+admin.site.register(ActivityType)
 admin.site.register(Activities)
