67edb66a07a490d77f95e0e02d1fc43a39bcac55
Mollygeorge <jessica.george@huskers.unl.edu>
2017-04-05 16:28:18 -0500

    Added activities to admin

--- schools/ranking/admin.py
+++ schools/ranking/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from ranking.models import TestType, TestScores, School, Enrollment
+from ranking.models import TestType, TestScores, School, Enrollment, Activities
 # Register your models here.
 admin.site.register(TestType)
 admin.site.register(TestScores)
 admin.site.register(School)
 admin.site.register(Enrollment)
+admin.site.register(Activities)
