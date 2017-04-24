bbd3723e429c5883331cabd2cf1c6d02e652805b
adrianjania <adrianjania543@gmail.com>
2017-04-24 17:50:10 -0500

    schools_list added for all test

--- schools/ranking/views.py
+++ schools/ranking/views.py
@@ -12,26 +12,26 @@ def schools_list(request):
     context = {'schools': schools, 'reading_scores': reading_scores}
     return render(request, 'ranking/schools_list.html', context)
 
+def schools_list(request):
+    schools = School.objects.order_by('name')
+    writing_scores = TestScores.objects.filter(test_type__type="NeSA Writing").order_by('school__name')
+    context = {'schools': schools, 'reading_scores': writing_scores}
+    return render(request, 'ranking/schools_list.html', context)
+
+def schools_list(request):
+    schools = School.objects.order_by('name')
+    math_scores = TestScores.objects.filter(test_type__type="NeSA Math").order_by('school__name')
+    context = {'schools': schools, 'math_scores': math_scores}
+    return render(request, 'ranking/schools_list.html', context)
+
+def schools_list(request):
+    schools = School.objects.order_by('name')
+    science_scores = TestScores.objects.filter(test_type__type="NeSA Science").order_by('school__name')
+    context = {'schools': schools, 'Science_scores': science_scores}
+    return render(request, 'ranking/schools_list.html', context)
+
 def schools_detail(request, pk):
     school = School.objects.get(id=pk)
     reading_score = TestScores.objects.filter(school=school).filter(test_type__type="NeSA Reading")
     context = {'school': school, 'reading_score': reading_score}
-    return render(request, 'ranking/schools_detail.html', context)
-
-def schools_detail(request, pk):
-    school = School.objects.get(id=pk)
-    reading_score = TestScores.objects.filter(school=school)
-    context = {'school': school, 'math_score': math_score}
-    return render(request, 'ranking/schools_detail.html', context)
-
-def schools_detail(request, pk):
-    school = School.objects.get(id=pk)
-    reading_score = TestScores.objects.filter(school=school)
-    context = {'school': school, 'science_score': science_score}
-    return render(request, 'ranking/schools_detail.html', context)
-
-def schools_detail(request, pk):
-    school = School.objects.get(id=pk)
-    reading_score = TestScores.objects.filter(school=school)
-    context = {'school': school, 'writing_score': writing_score}
     return render(request, 'ranking/schools_detail.html', context)
