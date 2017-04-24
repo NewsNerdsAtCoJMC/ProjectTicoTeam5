3c57f6aebc57db8573302551e7ef06f8a7bb73aa
Mollygeorge <jessica.george@huskers.unl.edu>
2017-04-24 14:53:27 -0500

    adjusted base, added homepage url/view/template, added school list url view and start of template.

--- schools/ranking/views.py
+++ schools/ranking/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from ranking.models import School
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'ranking/homepage.html', context)
+
+
+def schools_list(request):
+    schools = School.objects.order_by('name')
+    context = {'schools': schools}
+    return render(request, 'ranking/schools_list.html', context)
