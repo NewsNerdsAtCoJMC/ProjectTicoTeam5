9c56024fb1a9d882191640d1468c01cdde816efe
adrianjania <adrianjania543@gmail.com>
2017-04-25 12:41:29 -0500

    added frlunch, gifted, special ed

--- schools/ranking/views.py
+++ schools/ranking/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from ranking.models import School, TestScores
+from ranking.models import School, TestScores, Statistics
 
 def index(request):
     context = {}
@@ -13,6 +13,10 @@ def schools_list(request):
     math_scores = TestScores.objects.filter(test_type__type="NeSA Math").order_by('school__name')
     science_scores = TestScores.objects.filter(test_type__type="NeSA Science").order_by('school__name')
     frlunch = Statistics.objects.filter(stat_type__type="Frlunch").order_by('school__name')
+    gifted = Statistics.objects.filter(stat_type__type="Gifted").order_by('school__name')
+    specialed = Statistics.objects.filter(stat_type__type="Specialed").order_by('school__name')
+
+
     context = {'schools': schools, 'reading_scores': reading_scores, 'writing_scores': writing_scores, 'math_scores': math_scores, 'science_scores': science_scores, 'frlunch': frlunch}
     return render(request, 'ranking/schools_list.html', context)
 
