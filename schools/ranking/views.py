7a0bf9cf6326aab8a276c09e88eafedd08180333
adrianjania <adrianjania543@gmail.com>
2017-04-25 11:49:33 -0500

    eliminated unnecessary schools list functions

--- schools/ranking/views.py
+++ schools/ranking/views.py
@@ -9,32 +9,12 @@ def index(request):
 def schools_list(request):
     schools = School.objects.order_by('name')
     reading_scores = TestScores.objects.filter(test_type__type="NeSA Reading").order_by('school__name')
-    context = {'schools': schools, 'reading_scores': reading_scores}
-    return render(request, 'ranking/schools_list.html', context)
-
-def schools_list(request):
-    schools = School.objects.order_by('name')
     writing_scores = TestScores.objects.filter(test_type__type="NeSA Writing").order_by('school__name')
-    context = {'schools': schools, 'reading_scores': writing_scores}
-    return render(request, 'ranking/schools_list.html', context)
-
-def schools_list(request):
-    schools = School.objects.order_by('name')
     math_scores = TestScores.objects.filter(test_type__type="NeSA Math").order_by('school__name')
-    context = {'schools': schools, 'math_scores': math_scores}
-    return render(request, 'ranking/schools_list.html', context)
-
-def schools_list(request):
-    schools = School.objects.order_by('name')
     science_scores = TestScores.objects.filter(test_type__type="NeSA Science").order_by('school__name')
-    context = {'schools': schools, 'Science_scores': science_scores}
+    context = {'schools': schools, 'reading_scores': reading_scores, 'writing_scores': writing_scores, 'math_scores': math_scores, 'science_scores': science_scores}
     return render(request, 'ranking/schools_list.html', context)
 
-def schools_detail(request, pk):
-    school = School.objects.get(id=pk)
-    reading_score = TestScores.objects.filter(school=school).filter(test_type__type="NeSA Reading")
-    context = {'school': school, 'reading_score': reading_score}
-    return render(request, 'ranking/schools_detail.html', context)
 
 def schools_detail(request, pk):
     school = School.objects.get(id=pk)
