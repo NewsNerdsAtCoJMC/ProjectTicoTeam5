3a8aac88da6e374f6435bc11ec99f4cfd6ccc32d
adrianjania <adrianjania543@gmail.com>
2017-04-25 13:06:15 -0500

    reorder path to stats

--- schools/ranking/views.py
+++ schools/ranking/views.py
@@ -12,12 +12,8 @@ def schools_list(request):
     writing_scores = TestScores.objects.filter(test_type__type="NeSA Writing").order_by('school__name')
     math_scores = TestScores.objects.filter(test_type__type="NeSA Math").order_by('school__name')
     science_scores = TestScores.objects.filter(test_type__type="NeSA Science").order_by('school__name')
-    frlunch = Statistics.objects.filter(stat_type__type="Frlunch").order_by('school__name')
-    gifted = Statistics.objects.filter(stat_type__type="Gifted").order_by('school__name')
-    specialed = Statistics.objects.filter(stat_type__type="Specialed").order_by('school__name')
-
-
-    context = {'schools': schools, 'reading_scores': reading_scores, 'writing_scores': writing_scores, 'math_scores': math_scores, 'science_scores': science_scores, 'frlunch': frlunch, 'gifted': gifted, 'specialed': specialed}
+    stats= Statistics.objects.all().order_by('school__name')
+    context = {'schools': schools, 'reading_scores': reading_scores, 'writing_scores': writing_scores, 'math_scores': math_scores, 'science_scores': science_scores, 'stats':stats}
     return render(request, 'ranking/schools_list.html', context)
 
 
@@ -28,9 +24,6 @@ def schools_detail(request, pk):
     math_score = TestScores.objects.filter(school=school).filter(test_type__type="NeSA Math")
     science_score = TestScores.objects.filter(school=school).filter(test_type__type="NeSA Science")
     writing_score = TestScores.objects.filter(school=school).filter(test_type__type="NeSA Writing")
-    frlunch = Statistics.objects.filter(school=school).filter(stat_type__type="Frlunch")
-    gifted = Statistics.objects.filter(school=school).filter(stat_type__type="Gifted")
-    specialed = Statistics.objects.filter(school=school).filter(stat_type__type="Specialed")
-
-    context = {'school': school, 'reading_score': reading_score, 'math_score': math_score, 'science_score': science_score, 'writing_score': writing_score}
+    stats = Statistics.objects.filter(school=school)
+    context = {'school': school, 'reading_score': reading_score, 'math_score': math_score, 'science_score': science_score, 'writing_score': writing_score, 'stats':stats}
     return render(request, 'ranking/schools_detail.html', context)
